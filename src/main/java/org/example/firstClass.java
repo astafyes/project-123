5ccbdaf82f12922fa1ff541b4e6c201f312b4b98
Evgenia <speaknarcissus@gmail.com>
2025-04-24 15:43:19 +0300

    Добавлено использование метода doSomething

--- src/main/java/org/example/firstClass.java
+++ src/main/java/org/example/firstClass.java
@@ -4,6 +4,7 @@ public class firstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("1243");
+        doSomething();
     }
      public static void doSomething(){
         System.out.println("Hello World again");
