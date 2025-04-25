c3bb82b19b755244f83b3140507c2a0f47f81c1e
Evgenia <speaknarcissus@gmail.com>
2025-04-26 00:08:45 +0300

    Добавлена новая фраза в метод doSomething и добавили --- в первом блоке в начале

--- src/main/java/org/example/firstClass.java
+++ src/main/java/org/example/firstClass.java
@@ -2,11 +2,12 @@ package org.example;
 
 public class firstClass {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello World");
         System.out.println("1243");
         doSomething();
     }
      public static void doSomething(){
-        System.out.println("Hello World again");
+        System.out.println("Hello World no longer exists");
      }
 }
